5a8d2f93090589fcddce52d81edf2a828a72448b
Saurabh <Saurabh@Saurabh-PC>
2017-12-28 23:06:44 +0530

    Employee POJO

---
+++ src/main/java/com/java/hibernate/Employee.java
@@ -0,0 +1,61 @@
+package com.java.hibernate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="employee_criteria")
+public class Employee {	
+	@Id
+	@Column(name="id")
+	@GeneratedValue
+	int empId;
+	
+	@Column(name="emp_name")
+	String empName;
+	
+	@Column(name="salary")
+	int salary;
+
+	public int getEmpId() {
+		return empId;
+	}
+
+	public void setEmpId(int empId) {
+		this.empId = empId;
+	}
+
+	public String getEmpName() {
+		return empName;
+	}
+
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
+
+	public int getSalary() {
+		return salary;
+	}
+
+	public void setSalary(int salary) {
+		this.salary = salary;
+	}
+
+	
+
+	public Employee(int empId, String empName, int salary) {
+		super();
+		this.empId = empId;
+		this.empName = empName;
+		this.salary = salary;
+	}
+
+	public Employee() {
+		super();
+		// TODO Auto-generated constructor stub
+	}	
+	
+}
